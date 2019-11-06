09364ed147a67837b2988dddab26a85818f225ae
yassineCherifi <mohammedyassine.cherifi@student.univ-tlemcen.dz>
2019-11-06 20:00:04 +0100

    add editproject function and fix detail project component front

--- frontoffice/src/app/components/dashboard/detailprojet/detailprojet.component.ts
+++ frontoffice/src/app/components/dashboard/detailprojet/detailprojet.component.ts
@@ -29,6 +29,10 @@ export class DetailprojetComponent implements OnInit {
     status: ''
   }
 
+  modelproject = {
+    title: '',
+    description: ''
+  }
 
   ngOnInit() {   
     this.project_id = this.route.snapshot.paramMap.get('id');
@@ -37,7 +41,11 @@ export class DetailprojetComponent implements OnInit {
 
 
   getProject(){
-    this.projetService.getProject(this.project_id).subscribe(data => this.project = data['project']);
+    this.projetService.getProject(this.project_id).subscribe(data => {
+      this.project = data['project']
+      this.modelproject.title = this.project.title;
+      this.modelproject.description = this.project.description;
+    });
   }
 
   removeIssue(id){
@@ -57,9 +65,24 @@ export class DetailprojetComponent implements OnInit {
         console.log(err);
       }
     );
-    console.log(this.project)
   }
 
+  editProject(form: NgForm) {
+    console.log(form.value)
+    if(form.value.title === "" || form.value.description === ""){
+      this.getProject();
+      return;
+    }
+    this.projetService.editProject(this.project['_id'],form.value).subscribe(
+      res => {
+       this.getProject()
+  
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
 
 
 }

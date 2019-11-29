65c7e59e69f1687f4a020f4a86dd519b37ba72fb
Flavien <flavien.martineau@etu.u-bordeaux.fr>
2019-11-29 16:39:00 +0100

    Fix front uni test configuration

--- frontoffice/src/app/services/contributor.service.spec.ts
+++ frontoffice/src/app/services/contributor.service.spec.ts
@@ -1,9 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ContributorService } from './contributor.service';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ContributorService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientTestingModule,
+      HttpClientModule
+    ]
+  }));
 
   it('should be created', () => {
     const service: ContributorService = TestBed.get(ContributorService);
